6784a41a1393acc13bed8686d136e4dd49a10983
Bertrand Chenal <bch@b2ck.com>
2010-01-04 17:28:19 +0100

    Added french translation in __tryton__.py

--- __tryton__.py
+++ __tryton__.py
@@ -3,11 +3,13 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name' : 'Calendar Classification',
+    'name_fr_FR' : 'Classification calendrier',
     'version' : '0.0.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Handle classification of event',
+    'description_fr_FR': 'Gère la classification des évènements',
     'depends' : [
         'ir',
         'calendar',
@@ -15,5 +17,6 @@
     'xml' : [
     ],
     'translation': [
+        'fr_FR.csv',
     ],
 }
