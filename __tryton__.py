5649826544b8e238a40f0bed383cc9de8bc74184
Cédric Krier <ced@b2ck.com>
2011-04-27 12:20:40 +0200

    Prepare release 2.0.0

--- __tryton__.py
+++ __tryton__.py
@@ -9,7 +9,7 @@
     'name_es_ES' : 'Clasificación del calendario',
     'name_fr_FR' : 'Classification calendrier',
     'name_ru_RU' : 'Классификация по календарю',
-    'version' : '1.9.0',
+    'version' : '2.0.0',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
